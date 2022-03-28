96937cc5a537e212830e4f9a00940da404abd0b3
Matthias Güntert <matthias.guentert@gmail.com>
2022-03-28 20:50:39 +0200

    Initial commit

---
+++ src/main/java/io/azureblue/springbootservicebusdemo/SpringBootServiceBusDemoApplication.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootservicebusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServiceBusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServiceBusDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/azureblue/springbootservicebusdemo/SpringBootServiceBusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootservicebusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceBusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
